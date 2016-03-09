042fa0cc119553906c52ef53e57e22b5f65d2002
jing <mqjing@gmail.com>
2016-03-09 15:09:46 +0800

    add name utility and ip utility

--- name_utility.sh
+++ name_utility.sh
@@ -1,27 +1,54 @@
 #!/bin/sh
 
+# Library
+. ./ip_utility.sh
+
 # PATH TO YOUR HOSTS FILE
 ETC_HOSTS=/etc/hosts
 
-# DEFAULT IP FOR HOSTNAME
-IP="127.0.0.1"
 
-# Hostname to add/remove.
-HOSTNAME=$1
-
+# Function: removehost
+# Usage:
+#   removehost vm4
+# Return:
+#   0: success
+#   1: fail
 function removehost() {
+    local HOSTNAME=$1
+    if [ -z "$HOSTNAME" ]; then
+        echo "Usage: removehost HOSTNAME"
+        return 1
+    fi
+
     if [ -n "$(grep $HOSTNAME /etc/hosts)" ]
     then
         echo "$HOSTNAME Found in your $ETC_HOSTS, Removing now...";
         sudo sed -i".bak" "/$HOSTNAME/d" $ETC_HOSTS
+        return 0
     else
         echo "$HOSTNAME was not found in your $ETC_HOSTS";
+        return 1 
     fi
 }
 
+# Function: addhost
+# Usage:
+#   addhost vm4 10.109.62.233
+# Return:
+#   0: success
+#   1: fail
 function addhost() {
     HOSTNAME=$1
     IP=$2
+   
+    valid_ip ${IP}
+   
+    if [[ $? -eq 1 ]]
+    then 
+        echo "Usage: addhost hostname ip"
+        return 1
+    fi
+
     HOSTS_LINE="$IP\t$HOSTNAME"
     if [ -n "$(grep $HOSTNAME /etc/hosts)" ]
         then
